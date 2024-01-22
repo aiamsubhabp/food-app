197e89c7d380d17292e6ea9498153e2d3e95c7dd
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-01-21 19:31:49 -0500

    create routes.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import App from "./App";
+import FoodForm from "./components/FoodForm";
+
+const routes = [
+    {
+      path: "/",
+      element: <App />,
+      
+    }, 
+    {
+      path: "/about",
+      element: <About />,
+     
+    },
+  ];
+
+  export default routes
